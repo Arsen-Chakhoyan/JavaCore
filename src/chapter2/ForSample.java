6963c0583c6f5f8ad36d62843d1c5db272954c39
Arsen Chakhoyan <ars171098@gmail.com>
2023-08-25 21:52:10 +0400

    homework 1

--- src/chapter2/ForSample.java
+++ src/chapter2/ForSample.java
@@ -2,7 +2,7 @@ package chapter2;
 
 public class ForSample {
     public static void main(String[] args) {
-        for (int x = 0; x < 15; ++x) {
+        for (int x = 0; x < 10; ++x) {
             System.out.println("Значение x: " + x);
         }
     }
