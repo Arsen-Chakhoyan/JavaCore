d3c974f16efe48483f6c9f6d58aaf1c54cf67e11
Arsen Chakhoyan <ars171098@gmail.com>
2023-08-31 15:26:46 +0400

    homework 1

---
+++ src/chapter3/PlanetsArray.java
@@ -0,0 +1,19 @@
+package chapter3;
+
+public class PlanetsArray {
+    public static void main(String[] args) {
+        String planets [];
+
+        String planet = "Меркурий";
+        String planet1 = "Венера";
+        String planet2 = "Земля";
+        String planet3 = "Марс";
+        String planet4 = "Юпитер";
+        String planet5 = "Сатурн";
+        String planet6 = "Уран";
+        String planet7 = "Нептун";
+        String planet8 = "Плутон";
+        System.out.println(planet2 + " " + " это третья планета от солнца.");
+        System.out.println("Самая маленькая планета солнечной системы это " + planet );
+    }
+}
