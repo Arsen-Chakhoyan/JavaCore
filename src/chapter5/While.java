5911c9ae35869ce76b3f21fea284e5bf9dcc491c
Arsen Chakhoyan <ars171098@gmail.com>
2023-09-06 16:37:30 +0400

    homework 1

---
+++ src/chapter5/While.java
@@ -0,0 +1,11 @@
+package chapter5;
+
+public class While {
+    public static void main(String[] args) {
+        int t = 1;
+        while (t <= 15) {
+            System.out.println("такт "  + t);
+            t++;
+        }
+    }
+}
---
+++ src/myTests/StringsArray.java
@@ -0,0 +1,15 @@
+package myTests;
+
+public class StringsArray {
+    public static void main(String[] args) {
+        String weekNames;
+
+        String firstDay = "Sunday";
+        String secondDay = "Monday";
+        String thirdDay = "Tuesday";
+        String fourthDay = "Wednesday";
+        String fifthDay = "Friday";
+        String sixthDay = "Saturday";
+        System.out.println("In " + thirdDay + " we don`t work" );
+    }
+}
